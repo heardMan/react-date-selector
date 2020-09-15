84b7fc96e9eeb565eaa85dff8dfe809a7e4425c5
heardman <markyheard@gmail.com>
2020-09-14 20:40:04 -0700

    pushing production build for previous update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-date-selector/precache-manifest.ac26b0fd2b9e7df0ef0c906d075939df.js"
+  "/react-date-selector/precache-manifest.0a1d032be21abfc1e29615707e14a416.js"
 );
 
 self.addEventListener('message', (event) => {
