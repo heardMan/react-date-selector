a3ebbdaa55d8b255533fbf067f04e2743e76ac3a
heardman <markyheard@gmail.com>
2020-09-16 22:17:34 -0700

    enlarged modal

--- build/precache-manifest.059ff5d0c5790b8350fc4bf3a0cf638f.js
+++ build/precache-manifest.059ff5d0c5790b8350fc4bf3a0cf638f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ba488edd925bc0ad6d5f603e4727e661",
+    "revision": "bb10ec42b1fa22a4f3e69c3586bc5b69",
     "url": "/react-date-selector/index.html"
   },
   {
-    "revision": "a707864d085fdf5d8ac0",
-    "url": "/react-date-selector/static/css/main.d48f4d2c.chunk.css"
+    "revision": "87878bf2d9a9e2aa7fdc",
+    "url": "/react-date-selector/static/css/main.8efc8d7e.chunk.css"
   },
   {
     "revision": "98c7adff226d6fc5e30b",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-date-selector/static/js/2.78fdcc52.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a707864d085fdf5d8ac0",
+    "revision": "87878bf2d9a9e2aa7fdc",
     "url": "/react-date-selector/static/js/main.ed85ce49.chunk.js"
   },
   {
