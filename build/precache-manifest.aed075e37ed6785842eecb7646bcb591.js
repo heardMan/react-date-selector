bb3fdde1835c2f43542214de78d7f41606ec44bc
Mark Heard <markheard@Marks-MacBook-Pro.local>
2022-05-21 21:42:43 -0700

    commiting build code

---
+++ build/precache-manifest.aed075e37ed6785842eecb7646bcb591.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c7062c7bf233a36118c",
+    "url": "/react-date-selector/static/css/main.572e9c5b.chunk.css"
+  },
+  {
+    "revision": "8c7062c7bf233a36118c",
+    "url": "/react-date-selector/static/js/main.8c7062c7.chunk.js"
+  },
+  {
+    "revision": "8357abb205071e4fd2c4",
+    "url": "/react-date-selector/static/js/1.8357abb2.chunk.js"
+  },
+  {
+    "revision": "aa55e37d78111dae120b",
+    "url": "/react-date-selector/static/js/runtime~main.aa55e37d.js"
+  },
+  {
+    "revision": "2f97d706e9387587095950c34c562b0e",
+    "url": "/react-date-selector/static/media/arrow_forward_ios-white-18dp-2.2f97d706.svg"
+  },
+  {
+    "revision": "ac543f0dcd8cccaf55d7e7b8d2b4441c",
+    "url": "/react-date-selector/static/media/arrow_back_ios-white-18dp-2.ac543f0d.svg"
+  },
+  {
+    "revision": "d023c646564872407e9e1615ecf4c175",
+    "url": "/react-date-selector/static/media/double_arrow_forward_ios-white-18dp.d023c646.svg"
+  },
+  {
+    "revision": "1f4f4a42a8231362c35696c35fad5ca3",
+    "url": "/react-date-selector/static/media/double_arrow_back_ios-white-18dp.1f4f4a42.svg"
+  },
+  {
+    "revision": "dd6034838404971858db0acbf1fd6797",
+    "url": "/react-date-selector/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-date-selector/precache-manifest.dca654f36c73c16e42a837c9ca98d1d9.js"
+  "/react-date-selector/precache-manifest.aed075e37ed6785842eecb7646bcb591.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-date-selector/index.html"), {
+workbox.routing.registerNavigationRoute("/react-date-selector/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
